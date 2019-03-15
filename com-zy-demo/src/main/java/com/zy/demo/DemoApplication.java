8e954c5d86db0332a9ab19f3f1c535ba44bf1d1a
zhangya <zhang.ya@puscene.com>
2019-03-15 19:12:25 +0800

    modify

--- com-zy-demo/src/main/java/com/zy/demo/DemoApplication.java
+++ com-zy-demo/src/main/java/com/zy/demo/DemoApplication.java
@@ -4,7 +4,7 @@ package com.zy.demo;
  * Hello world!
  *
  */
-public class App {
+public class DemoApplication {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
